2e633dbaef5817d20899a7c8e94922aa08ebd548
Cintia Higashi <cintiamh@gmail.com>
2016-04-25 00:05:59 -0700

    included CSS to Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,47 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var config = {
-    entry: [
-        'webpack/hot/dev-server',
-        'webpack-dev-server/client?http://localhost:3000',
-        './src/index.js'
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:3000',
+    './src/index.js'
+  ],
+  resolve: {
+    root: [
+      // allows us to import modules as if /src was the root.
+      // so I can do: import Comment from 'components/Comment'
+      // instead of:  import Comment from '../components/Comment' or whatever relative path would be
+      path.resolve(__dirname, './src')
     ],
-    resolve: {
-        root: [
-            // allows us to import modules as if /src was the root.
-            // so I can do: import Comment from 'components/Comment'
-            // instead of:  import Comment from '../components/Comment' or whatever relative path would be
-            path.resolve(__dirname, './src')
-        ],
-        // allows you to require without the .js at end of filenames
-        // import Component from 'component' vs. import Component from 'component.js'
-        extensions: ['', '.js', '.json', '.jsx']
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['babel'],
-                exclude: /(node_modules|bower_components)/
-            }
-        ]
-    }
+    // allows you to require without the .js at end of filenames
+    // import Component from 'component' vs. import Component from 'component.js'
+    extensions: ['', '.js', '.json', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+      }
+    ]
+  }, plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
 };
 
 module.exports = config;
