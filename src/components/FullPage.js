73ea86ba97e75eee71cc12896d69355fcc952dea
Cintia Higashi <cintiamh@gmail.com>
2016-04-27 23:06:26 -0700

    centering landing page

--- src/components/FullPage.js
+++ src/components/FullPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const FullPage = () => (
-    <div className="full-page landing">
-        <h1>Cintia Higashi</h1>
-        <h2>Front End Software Engineer</h2>
-        <button>Check out my work!</button>
+    <div className="full-page">
+        <div className="landing">
+          <h1>Cintia Higashi</h1>
+          <h2>Front End Software Engineer</h2>
+          <button>Check out my work!</button>
+        </div>
     </div>
 );
 
