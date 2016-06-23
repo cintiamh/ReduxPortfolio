45d868ffcdce53ea64ffcca35581ebda19f96cac
Cintia Higashi <cintiamh@gmail.com>
2016-06-23 08:24:51 -0700

    Fake list of projects

---
+++ src/config/projectsList.js
@@ -0,0 +1,70 @@
+const projectsList = [
+    {
+        id: 0,
+        label: 'Snakes 2D',
+        image: 'http://cintiamh.github.io/img/projects/snakes2d.png',
+        github: 'https://github.com/cintiamh/snakes2d',
+        demo: 'http://cintiamh.github.io/snakes2d/',
+        description: `This is a simple HTML5 Snakes game.\n
+            The game’s grid size is responsible, resizing automatically according to the browser’s window size.\n
+            The snake grows up one “square” every time it eats an apple (red circle) and also speeds up a little bit each time.\n
+            Commands are the directional arrows.`
+    },
+    {
+        id: 1,
+        label: 'Snakes 2D',
+        image: 'http://cintiamh.github.io/img/projects/snakes2d.png',
+        github: 'https://github.com/cintiamh/snakes2d',
+        demo: 'http://cintiamh.github.io/snakes2d/',
+        description: `This is a simple HTML5 Snakes game.\n
+            The game’s grid size is responsible, resizing automatically according to the browser’s window size.\n
+            The snake grows up one “square” every time it eats an apple (red circle) and also speeds up a little bit each time.\n
+            Commands are the directional arrows.`
+    },
+    {
+        id: 2,
+        label: 'Snakes 2D',
+        image: 'http://cintiamh.github.io/img/projects/snakes2d.png',
+        github: 'https://github.com/cintiamh/snakes2d',
+        demo: 'http://cintiamh.github.io/snakes2d/',
+        description: `This is a simple HTML5 Snakes game.\n
+            The game’s grid size is responsible, resizing automatically according to the browser’s window size.\n
+            The snake grows up one “square” every time it eats an apple (red circle) and also speeds up a little bit each time.\n
+            Commands are the directional arrows.`
+    },
+    {
+        id: 3,
+        label: 'Snakes 2D',
+        image: 'http://cintiamh.github.io/img/projects/snakes2d.png',
+        github: 'https://github.com/cintiamh/snakes2d',
+        demo: 'http://cintiamh.github.io/snakes2d/',
+        description: `This is a simple HTML5 Snakes game.\n
+            The game’s grid size is responsible, resizing automatically according to the browser’s window size.\n
+            The snake grows up one “square” every time it eats an apple (red circle) and also speeds up a little bit each time.\n
+            Commands are the directional arrows.`
+    },
+    {
+        id: 4,
+        label: 'Snakes 2D',
+        image: 'http://cintiamh.github.io/img/projects/snakes2d.png',
+        github: 'https://github.com/cintiamh/snakes2d',
+        demo: 'http://cintiamh.github.io/snakes2d/',
+        description: `This is a simple HTML5 Snakes game.\n
+            The game’s grid size is responsible, resizing automatically according to the browser’s window size.\n
+            The snake grows up one “square” every time it eats an apple (red circle) and also speeds up a little bit each time.\n
+            Commands are the directional arrows.`
+    },
+    {
+        id: 5,
+        label: 'Snakes 2D',
+        image: 'http://cintiamh.github.io/img/projects/snakes2d.png',
+        github: 'https://github.com/cintiamh/snakes2d',
+        demo: 'http://cintiamh.github.io/snakes2d/',
+        description: `This is a simple HTML5 Snakes game.\n
+            The game’s grid size is responsible, resizing automatically according to the browser’s window size.\n
+            The snake grows up one “square” every time it eats an apple (red circle) and also speeds up a little bit each time.\n
+            Commands are the directional arrows.`
+    }
+];
+
+export default projectsList;
